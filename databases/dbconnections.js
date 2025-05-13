54ad350a7460275cd3efb7f8c21d854b51a8e3fd
MohamedAnsary14 <mohammedansary911@gmail.com>
2025-05-13 17:30:35 +0300

    mongo

--- databases/dbconnections.js
+++ databases/dbconnections.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbconnection = () => {
 
-    return mongoose.connect(process.env.DB_ONLINE).then(() => {
+    return mongoose.connect(process.env.mongodbUrl).then(() => {
         console.log('Database Connected');
     }).catch((err) => {
         console.log("DataBase Error", err);
